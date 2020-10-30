64b3d5f338258a95d8c58a46cd6db19d2a1b9b30
Dev Thay <67610492+thaydev@users.noreply.github.com>
2020-10-30 09:48:44 -0300

    Add files via upload

---
+++ vetor - 6de40.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(){
+    //entender o vetor
+    int vetor[]={1,2,3,4,5},i;
+    
+    for(i=0;i<5;i++){
+      printf("%d\n",vetor[i]);	
+	}
+    
+    
+	return 0;
+}
